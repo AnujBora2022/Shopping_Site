9a8e71ae3bd5eee584c1fc3b7135847c2b0b359f
AnujBora2022 <121304516+AnujBora2022@users.noreply.github.com>
2024-09-30 21:23:26 +0530

    buildVercel

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,11 @@
 echo 'Building the project...'
-python3.9 -m pip install -r requirements.txt
+pip install setuptools
+pip -m pip install -r requirements.txt
+
 
 # Run Django migrations (optional but recommended)
 echo 'Make Migrations'
-python3.9 manage.py make migrations --noinput
+python manage.py make migrations --noinput
 python manage.py migrate
 
 # Collect static files
