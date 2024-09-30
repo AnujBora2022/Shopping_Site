347706116c2b4f324c7ad67aaca391a53444d409
AnujBora2022 <121304516+AnujBora2022@users.noreply.github.com>
2024-09-30 21:31:21 +0530

    buildVercel2

--- build_files.sh
+++ build_files.sh
@@ -5,8 +5,8 @@ python3.12 install -r requirements.txt
 
 # Run Django migrations (optional but recommended)
 echo 'Make Migrations'
-python manage.py make migrations --noinput
-python manage.py migrate
+python3.12 manage.py make migrations --noinput
+python3.12 manage.py migrate
 
 # Collect static files
-python manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
