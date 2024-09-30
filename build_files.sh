6ecca1b856bcc39bdb05bbd96e9d7016d2afb6d3
AnujBora2022 <121304516+AnujBora2022@users.noreply.github.com>
2024-09-30 20:44:29 +0530

    Updated build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,10 @@
-pip install -r requirements.txt
+echo 'Building the project...'
+python3.9 -m pip install -r requirements.txt
 
 # Run Django migrations (optional but recommended)
+echo 'Make Migrations'
+python3.9 manage.py make migrations --noinput
 python manage.py migrate
 
 # Collect static files
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
